741cdbefa37242256b52dcbfbf559bd8ad0cbc05
AYSung <aysung300@gmail.com>
2014-11-04 21:20:46 -0500

    Started work on 'Goal' class

---
+++ src/cpsc112/cesspool/Goal.java
@@ -0,0 +1,18 @@
+package cpsc112.cesspool;
+
+import java.util.*;
+import java.text.*;
+
+public class Goal {
+	//declare fields
+	protected String name;
+	protected double value;
+	protected Date dueDate;
+	protected DateFormat dateFormat;
+	
+	//constructor for class 'Goal'
+	public Goal(){
+		
+	}
+
+}
