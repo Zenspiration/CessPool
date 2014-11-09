0c7815c66c90e4388cd4df29717febac66223a28
AYSung <aysung300@gmail.com>
2014-11-09 14:27:45 -0500

    Added comments for all code

--- src/cpsc112/cesspool/Member.java
+++ src/cpsc112/cesspool/Member.java
@@ -3,41 +3,48 @@ package cpsc112.cesspool;
 import java.util.ArrayList;
 
 public class Member {
-	protected String name;
-	protected boolean leader;
-	protected ArrayList<Goal> goals;
-	protected int dailyEarning, cycleEarning, totalPool;
+	//variables that belong to each 'member'
+	protected String userName, userID; //username, unique ID
+	protected boolean maker; //is leader (true/false)
+	protected ArrayList<Goal> goals; //collection of goals for the user
+	protected int dailyEarning, cycleEarning; //money earned that day, money earned during cycle
 
-	public Member(String name){
-		this.leader = false;
-		this.name = name;
+	public Member(String userID){
 		this.goals = new ArrayList<Goal>();
+		//add code here to set values of above fields based on properties of the circle
+		
+		//add code here to create database entry for member
+		//i.e. store the above variables in the database
 	}
 	
-	public boolean isLeader(){
-		return leader;
+	public boolean isMaker(){
+		return maker; //change to return true/false based on query with database
 	}
 	
-	public void setLeader(boolean b){
-		leader = b;
+	public void setMaker(boolean b){
+		maker = b; //set whether or not this member is the maker of the circle
+		//add code to set database value also equal to parameter
 	}
 	
 	public String getName(){
-		return name;
+		return userName; //change to return userName based on query with database
 	}
 	
 	public void setName(String name){
-		this.name = name;		
+		this.userName = name;		
 	}
 	
 	public void addGoal(Goal newGoal){
-		this.goals.add(newGoal);
+		this.goals.add(newGoal); //add goal to collection of goals
+		//add code to also add the goal to database
 	}
 	
 	public void removeDoneGoals(){
+		//goes through collection of goals to remove goals marked as 'done'
 		for (Goal g : goals){
 			if (g.done == true){
 				goals.remove(g);
+				//add code to also remove from database
 			}
 		}
 	}
--- src/cpsc112/cesspool/Pool.java
+++ src/cpsc112/cesspool/Pool.java
@@ -1,13 +1,27 @@
 package cpsc112.cesspool;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class Pool {
-	private String name;
-	private ArrayList<Member> members;
+	private String name, poolID;
+	private GregorianCalendar startDate, endDate;
+	private ArrayList<Member> members; //list of members
 
-	public Pool(){
-		members = new ArrayList<Member>();
+	public Pool(String name, GregorianCalendar startDate, GregorianCalendar endDate){
+		//initializes fields
+		this.name = name;
+		this.poolID = "000001"; //change to create unique ID for each new pool
+		this.members = new ArrayList<Member>();
+		this.startDate = startDate;
+		this.endDate = endDate;
+		
+		//add code to set database values to those from above
+		
+	}
+	
+	public Pool(String poolID){
+		//get values from database from existing pool using it's ID
 	}
 	
 	//gets name of pool
@@ -20,17 +34,13 @@ public class Pool {
 		this.name = name;
 	}
 	
-	//adds new member to pool
-	public void addMember(Member newMember){
-		this.members.add(newMember);
+	public ArrayList<Member> getMembers(){
+		return new ArrayList<Member>(); //change to create arraylist of members from database query (for loop through each member entry?)
 	}
 	
-	public boolean isMember(String name){
-		for (Member m: members){
-			if (m.getName().equals(name)){
-				return true;
-			}
-		}
-		return false;
+	//adds new member to pool
+	public void addMember(Member newMember){ //e.g. addMember(new Member(00002)); will add userID 00002 as a member of the pool
+		this.members.add(newMember);
+		//add code to add the member to the pool database
 	}
 }
